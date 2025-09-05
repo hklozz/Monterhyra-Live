4ff89ee877971b0ec90af52205a9d71e7acb344c
Hampus Klozz <Hampus@nmeproduction.se>
2025-09-05 09:55:43 +0200

    Lägg till 75" beTV (11000kr) och ändra 70" TV till 5500kr

--- src/App.tsx
+++ src/App.tsx
@@ -175,7 +175,8 @@ const TV_SIZES = [
   { label: '32"', width: 0.71, height: 0.40, image: '/Models/tvs/32inch.svg' },
   { label: '43"', width: 0.96, height: 0.56, image: '/Models/tvs/43inch.svg' },
   { label: '55"', width: 1.22, height: 0.71, image: '/Models/tvs/55inch.svg' },
-  { label: '70"', width: 1.55, height: 0.90, image: '/Models/tvs/70inch.svg' }
+  { label: '70"', width: 1.55, height: 0.90, image: '/Models/tvs/70inch.svg' },
+  { label: '75" beTV', width: 1.68, height: 0.95, image: '/Models/tvs/75inch.svg' }
 ];
 
 const TRUSS_TYPES = [
@@ -2282,7 +2283,8 @@ export default function App() {
       if (tvConfig.label === '32"') totalPrice += 2000;
       else if (tvConfig.label === '43"') totalPrice += 2500;
       else if (tvConfig.label === '55"') totalPrice += 3500;
-      else if (tvConfig.label === '70"') totalPrice += 10000;
+      else if (tvConfig.label === '70"') totalPrice += 5500;
+      else if (tvConfig.label === '75" beTV') totalPrice += 11000;
       // "Ingen" TV kostar inget (tvConfig.label === 'Ingen')
     });
     
